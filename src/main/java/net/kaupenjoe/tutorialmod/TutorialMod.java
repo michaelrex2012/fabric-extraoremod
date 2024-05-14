c13091797ce2193b009f766c3706af8d48623658
Michael <michaelgiu2012@outlook.com>
2024-05-14 17:52:26 -0400

    Initial commit

---
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
@@ -0,0 +1,22 @@
+package net.kaupenjoe.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.kaupenjoe.tutorialmod.block.ModBlocks;
+import net.kaupenjoe.tutorialmod.item.ModItemGroups;
+import net.kaupenjoe.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
