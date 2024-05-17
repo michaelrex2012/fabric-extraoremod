7a045fc279cddcea364e79f17fc3749646acd92a
Michael <michaelgiu2012@outlook.com>
2024-05-17 17:57:48 -0400

    Updated

--- src/main/java/net/michael/eom/item/ModItemGroups.java
+++ src/main/java/net/michael/eom/item/ModItemGroups.java
@@ -21,8 +21,6 @@ public class ModItemGroups {
 
                         entries.add(ModItems.METAL_DETECTOR);
 
-                        entries.add(Items.DIAMOND);
-
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
 
