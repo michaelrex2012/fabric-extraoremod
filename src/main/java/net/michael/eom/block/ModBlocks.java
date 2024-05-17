3cbcdde69a15001cdd4d70bc8d285c3d033bf845
Michael <michaelgiu2012@outlook.com>
2024-05-17 09:01:25 -0400

    Updated

--- src/main/java/net/michael/eom/block/ModBlocks.java
+++ src/main/java/net/michael/eom/block/ModBlocks.java
@@ -21,13 +21,13 @@ public class ModBlocks {
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
     public static final Block RUBY_ORE = registerBlock("ruby_ore",
-            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(2f), UniformIntProvider.create(2, 5)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),FabricBlockSettings.copyOf(Blocks.STONE).strength(2f)));
     public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
-            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(4f), UniformIntProvider.create(2, 5)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(4f)));
     public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
-            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHERRACK).strength(1.5f), UniformIntProvider.create(2, 5)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), FabricBlockSettings.copyOf(Blocks.NETHERRACK).strength(1.5f)));
     public static final Block END_STONE_RUBY_ORE = registerBlock("end_stone_ruby_ore",
-            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.END_STONE).strength(4f), UniformIntProvider.create(4, 7)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(4, 7), FabricBlockSettings.copyOf(Blocks.END_STONE).strength(4f)));
 
 
     private static Block registerBlock(String name, Block block) {
--- src/main/java/net/michael/eom/item/ModItems.java
+++ src/main/java/net/michael/eom/item/ModItems.java
@@ -1,6 +1,5 @@
 package net.michael.eom.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.michael.eom.TutorialMod;
@@ -12,11 +11,11 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
-    public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+    public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new Item.Settings()));
 
     public static final Item METAL_DETECTOR = registerItem("metal_detector",
-            new MetalDetectorItem(new FabricItemSettings().maxDamage(32)));
+            new MetalDetectorItem(new Item.Settings().maxDamage(32)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
